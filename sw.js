a0ebd053b8c54238f36ada3d50722097fdac5e71
bright-ic <40720995+bright-ic@users.noreply.github.com>
2018-10-07 10:21:56 +0100

    Update on sw.js - changed assests path from relative path to absolute path.

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-self.importScripts('/js/idb.js'); // add the idb library
+self.importScripts('./js/idb.js'); // add the idb library
 const filesToCache = [
-    "/",
-    "/index.html",
-    "/restaurant.html",
-    "/css/styles.css",
+    "./",
+    "./index.html",
+    "./restaurant.html",
+    "./css/styles.css",
     //"/data/restaurants.json",
-    "/js/dbhelper.js",
-    "/js/main.js",
-    "/js/restaurant_info.js",
-    "/img/1.jpg",
-    "/img/2.jpg",
-    "/img/3.jpg",
-    "/img/4.jpg",
-    "/img/5.jpg",
-    "/img/6.jpg",
-    "/img/7.jpg",
-    "/img/8.jpg",
-    "/img/9.jpg",
-    "/img/10.jpg"
+    "./js/dbhelper.js",
+    ".js/main.js",
+    "./js/restaurant_info.js",
+    "./img/1.jpg",
+    "./img/2.jpg",
+    "./img/3.jpg",
+    "./img/4.jpg",
+    "./img/5.jpg",
+    "./img/6.jpg",
+    "./img/7.jpg",
+    "./img/8.jpg",
+    "./img/9.jpg",
+    "./img/10.jpg"
 ];
 const appCacheName = "resturant-review-app-cache-v1";
 let dbPromise;
